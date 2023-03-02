80a5bad2dd7415774f63a1d233f51902154b8534
Ambati Pradeep Reddy <100661597+apr61@users.noreply.github.com>
2023-03-02 13:46:24 +0000

    Updated Profile component

--- src/pages/accountProfilePage/AccountProfilePage.jsx
+++ src/pages/accountProfilePage/AccountProfilePage.jsx
@@ -2,48 +2,53 @@ import React, { useRef, useState } from 'react'
 import { useAuthContext } from '../../context/AuthContext'
 import './accountProfilePage.css'
 import Input from '../../components/inputFields/Input'
+import { updateUserProfile } from '../../services/auth'
 
 function AccountProfilePage() {
-	const { currentUser } = useAuthContext()
-	const [isEditing, setIsEditing] = useState(false)
-	const nameRef = useRef()
-	const emailRef = useRef()
-	const passwordRef = useRef()
-	const confirmPasswordRef = useRef()
+  const { currentUser } = useAuthContext()
+  const [isEditing, setIsEditing] = useState(false)
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const confirmPasswordRef = useRef()
 
-	function updateUserProfile(){
+  function handleUpdateUserProfile() {
+    const newUser = {
+      displayName: nameRef.current.value
+    }
+    updateUserProfile(newUser, currentUser)
+    setIsEditing(false)
+  }
 
-	}
-
-	return (
-		<section className="profile main">
-			<h2 className="profile__title">Profile</h2>
-			<div className="profile__body">
-				<Input labelName={"Name"} ref={nameRef} value={currentUser.displayName} readOnly={!isEditing} />
-				<Input labelName={"Email"} ref={emailRef} value={currentUser.email} readOnly={!isEditing} />
-				{
-					isEditing && (
-						<>
-							<Input labelName={"Password"} ref={passwordRef} readOnly={!isEditing} />
-							<Input labelName={"Confirm password"} ref={confirmPasswordRef} readOnly={!isEditing} />
-						</>
-					)
-				}
-				<div className="profile__btn-container">
-					{
-						isEditing ? (
-							<>
-								<button className="profile__btn profile__btn--cancel" onClick={() => setIsEditing(!isEditing)}>Cancel</button>
-								<button className="profile__btn" onClick={updateUserProfile}>Update</button>
-							</>
-						) : (
-							<button className="profile__btn" onClick={() => setIsEditing(!isEditing)}>Edit</button>
-						)
-					}
-				</div>
-			</div>
-		</section>
-	)
+  return (
+    <section className="profile main">
+      <h2 className="profile__title">Profile</h2>
+      <div className="profile__body">
+        <Input labelName={"Name"} ref={nameRef} value={currentUser.displayName} readOnly={!isEditing} />
+        <Input labelName={"Email"} ref={emailRef} value={currentUser.email} readOnly={!isEditing} />
+        {
+          isEditing && (
+            <>
+              <Input labelName={"Password"} ref={passwordRef} readOnly={!isEditing} />
+              <Input labelName={"Confirm password"} ref={confirmPasswordRef} readOnly={!isEditing} />
+            </>
+          )
+        }
+        <div className="profile__btn-container">
+          {
+            isEditing ? (
+              <>
+                <button className="profile__btn profile__btn--cancel" onClick={() => setIsEditing(!isEditing)}>Cancel</button>
+                <button className="profile__btn" onClick={handleUpdateUserProfile}>Update</button>
+              </>
+            ) : (
+              <button className="profile__btn" onClick={() => setIsEditing(!isEditing)}>Edit</button>
+            )
+          }
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default AccountProfilePage
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,14 +1,21 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth"
+
 import { auth, db } from "../FirebaseConfig"
 
-export const signUp = async(email,password) => {
-    return await createUserWithEmailAndPassword(auth, email, password)
+export const signUp = async (email, password) => {
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
 
-export const signIn = async(email, password) => {
-    return await signInWithEmailAndPassword(auth, email, password)
+export const signIn = async (email, password) => {
+  return await signInWithEmailAndPassword(auth, email, password)
 }
 
 export const logOut = async () => {
-    return await signOut(auth)
+  return await signOut(auth)
+}
+
+export const updateUserProfile = async (user, currentUser) => {
+  await updateProfile(currentUser, {
+    displayName: user.displayName
+  })
 }
