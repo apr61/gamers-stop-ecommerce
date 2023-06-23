fccbff56528098b4d0e7dd3351605b2ca5f6e05b
Ambati Pradeep Reddy <100661597+apr61@users.noreply.github.com>
2023-06-23 05:10:53 +0000

    refactor - styles

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,18 +3,22 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(localStorage.getItem("gamers-stop-theme") || "light");
+  const [theme, setTheme] = useState(
+    localStorage.getItem("gamers-stop-theme") || "light"
+  );
   function toggleTheme() {
-    if(theme === "light"){
-      setTheme("dark")
-    }else{
-      setTheme("light")
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
     }
   }
   useEffect(() => {
-    theme === 'dark' ? document.body.classList.add("dark") : document.body.classList.remove("dark")
-    localStorage.setItem('gamers-stop-theme', theme)
-  }, [theme])
+    theme === "dark"
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+    localStorage.setItem("gamers-stop-theme", theme);
+  }, [theme]);
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
